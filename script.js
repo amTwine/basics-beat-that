8988126dbccb5c356a8e36386ee89eb4efe88535
amTwine <91801047+amTwine@users.noreply.github.com>
2021-10-30 01:50:25 -0400

    beat that update

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+//generate ramdom#
+var generateDiceNumber = function () {
+  var diceNumber = Math.ceil(Math.random() * 6);
+  return diceNumber();
 };
+
+// main function
+var main = function (input) {
+  var randomDiceRoll = diceNumber();
+  var randomDiceRoll2 = diceNumber();
+  console.log(
+    "random dice 1 " + randomDiceRoll,
+    "random dice 2 " + randomDiceRoll2
+  );
+  var myOutputValue = "";
+};
+// output: player playing, dice rolls for player
